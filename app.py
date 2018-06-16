94e2a1a6986f53e1d8655d6b97849efa5e04e5ba
Paul G. Arias <paul.g.arias@gmail.com>
2018-06-16 13:55:00 -0400

    Added the files so that we can query from database.

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
 from flask import Flask, render_template, jsonify, redirect
 from flask_restful import Resource, Api
 
+from sqlalchemy import Column, Float, Integer, String, Text, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+
 import json
+
+Base = declarative_base()
+
+class myJson(Base):
+    __tablename__ = "myjson"
+    jsonid = Column(Integer, primary_key=True)
+    json = Column(Text)
+
+engine = create_engine("sqlite:///myjson.sqlite")
+Base.metadata.create_all(engine)
+
+session = Session(bind=engine)
 
 app = Flask(__name__)
 api = Api(app)
 
 @app.route('/')
 def index():
-	return render_template("index.html")
+    return render_template("index.html")
 
 
 @app.route('/us-states')
 def us_states():
-	f = open("us-states.json",'r')
-	us_state_json = json.loads(f.read())
-	f.close()
-	return jsonify(us_state_json)
+    data = session.query(myJson.json).filter(myJson.jsonid==2).all()
+    return jsonify(json.loads(data[0][0]))
 
 @app.route('/usa-jobs')
 def usa_jobs():
-	f = open("usaJobs.json",'r')
-	usa_jobs_json = json.loads(f.read())
-	f.close()
-	return jsonify(usa_jobs_json)
+    data = session.query(myJson.json).filter(myJson.jsonid==1).all()
+    return jsonify(json.loads(data[0][0]))
 
 if __name__ == "__main__":
     app.run(debug=True)
