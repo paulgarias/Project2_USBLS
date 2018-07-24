863813653ddc20daa9d0eb58b2b97521206328be
Paul G. Arias <paul.g.arias@gmail.com>
2018-07-24 13:50:29 -0400

    Added a correction to convert back the --- to / in the API resource.

--- app.py
+++ app.py
@@ -47,6 +47,7 @@ def usa_jobs():
 
 class JobsHistory(Resource):
     def get(self, state, occTitle):
+	occTitle = occTitle.replace('---','/')
         data = session_hist.query(jobsDB.year,jobsDB.tot_emp,jobsDB.state).filter(jobsDB.state==state,jobsDB.occ_title==occTitle).all()
         jsonitem = [{"year": item[0], "value":item[1]} for item in data]
         return jsonify(jsonitem)
